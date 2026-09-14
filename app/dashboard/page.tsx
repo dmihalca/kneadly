391ca4e85dcd8aef0daed6626db16ddeef1ca763
dmihalca <158750950+dmihalca@users.noreply.github.com>
2026-09-14 20:02:59 +0200

    verlce build error fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { SiteHeader } from '@/components/site-header'
 import DoughCalculator from '@/components/dough-calculator'
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col min-w-0">
